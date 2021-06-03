f5b28cfe5b8608877b2236abbb5706fd270155f0
ejolie <ejolie.dev@gmail.com>
2021-06-04 00:28:32 +0900

    feat: Footer

---
+++ homework/eunjeongpark/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <p class="text-center text-grey text-xs">
+    ©2021 Captain Pangyo. All rights reserved.
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
